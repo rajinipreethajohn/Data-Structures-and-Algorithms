192b9ab4272689b1c92ec0f99dfcf60fc98a2c65
rajinipreethajohn <rajinipreethaj@hotmail.com>
2023-03-24 14:13:02 +0800

    24Mar 2nd commit

---
+++ selection_sort.py
@@ -0,0 +1,22 @@
+import sys
+from load import load_numbers
+
+numbers = load_numbers(sys.argv[1])
+
+def selection_sort(values):
+    sorted_values = []
+    print("%-25s %-25s" % (values, sorted_values)) 
+    for i in range(0,len(values)):
+        index_to_move = index_of_min(values)
+        sorted_values.append(values.pop(index_to_move))
+        print("%-25s %-25s" % (values, sorted_values))
+    return sorted_values
+
+def index_of_min(values):
+    min_index = 0
+    for i in range(1, len(values)):
+        if values[i] < values[min_index]:
+            min_index = i
+    return min_index
+    
+print(selection_sort(numbers))
